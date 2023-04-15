57c86f183ec1af765ccf47f646d046dd2b382ba6
yaroofie <yaroofie@gmail.com>
2023-04-15 13:00:45 +0330

    chore : install @fancyapps/ui and toastr

--- app.vue
+++ app.vue
@@ -5,8 +5,16 @@
 </template>
 
 <script setup>
+import { Fancybox } from "@fancyapps/ui";
+onMounted(() => {
+  Fancybox.bind("[data-fancybox]");
+})
 useRouter().beforeEach( () =>
 {
   useNuxtApp().$destroyLocomotive();
-} )
+} );
+useRouter().afterEach( () =>
+{
+  Fancybox.bind("[data-fancybox]");
+})
 </script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,18 @@
 <template>
   <div data-scroll-container>
-    <div data-scroll-section class="flex flex-col justify-center items-center gap-8">
+    <div
+      data-scroll-section
+      class="flex flex-col justify-center items-center gap-8"
+    >
       <h2 data-scroll data-scroll-speed="1">About page</h2>
-      <nuxt-img src="/images/4k.jpg" sizes="sm:100vw md:50vw lg:800px" />
+      <nuxt-img
+        src="/images/4k.jpg"
+        sizes="sm:100vw md:50vw lg:800px"
+        data-fancybox
+        data-caption="Caption #1"
+        data-src="/images/4k.jpg"
+      />
+      <button class="btn" @click="toastMe">taost</button>
       <!-- {{ store.count }} -->
       <FormKit type="form">
         <FormKit
@@ -23,7 +33,7 @@
           :options="['Warrior', 'Mage', 'Assassin']"
         />
       </FormKit>
-      <nuxt-img src="/images/4k.jpg" sizes="sm:100vw md:50vw lg:800px" lazy/>
+      <nuxt-img src="/images/4k.jpg" sizes="sm:100vw md:50vw lg:800px" lazy />
       <NuxtLink :to="localPath('/')" class="mb-20">
         <button class="btn btn-primary">Back to home</button>
       </NuxtLink>
@@ -32,8 +42,12 @@
 </template>
 
 <script setup>
+import Toastr from "toastr";
 const localPath = useLocalePath();
 onMounted(() => {
-  if ( process.client ) useNuxtApp().$initLocomotive();
-} );
+  if (process.client) useNuxtApp().$initLocomotive();
+});
+function toastMe() {
+  Toastr.success("Have fun storming the castle!", "Miracle Max Says");
+}
 </script>
